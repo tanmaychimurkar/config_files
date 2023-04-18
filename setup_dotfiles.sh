018ea4ccda70e15e158373f6a08693ac2c6503db
Tanmay Chimurkar <tanmay.chimurkar@uzh.ch>
2023-04-18 13:21:56 +0200

    Updating the setup bash script

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -2,8 +2,12 @@
 
 # Script to copy dotfiles from the repo to the home directory
 
+echo "Configuration files will be copied now to the $HOME directory of this machine"
+
 # Path to the dotfiles repo
 DOTFILES_REPO="$HOME/dotfiles"
+
+echo "Dotfiles directory is $DOTFILES_REPO"
 
 # Path to the home directory
 HOME_DIR="$HOME"
@@ -11,8 +15,15 @@ HOME_DIR="$HOME"
 # List of dotfiles to copy
 DOTFILES=(bashrc bash_aliases ideavimrc gitconfig tmux.conf )
 
+echo "List of dotfiles that will be copied are $DOTFILES"
+
 # Copy dotfiles to the home directory
 for file in "${DOTFILES[@]}"; do
-    mv "$file" ".$file"
-    cp -r "$DOTFILES_REPO/.$file" "$HOME_DIR"
+    cp -r "$DOTFILES_REPO/$file" "$HOME_DIR"
+    mv "$HOME/$file" "$HOME/.$file"
 done
+
+echo "The shell will be reloaded now"
+exec $SHELL
+
+echo "All dotfiles are now copied (except JetBrains configurations). You are good to go"
