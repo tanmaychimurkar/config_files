a8eafd7bc114c537406b317d8b0041fc00eda3b5
Tanmay Chimurkar <tanmay.chimurkar@uzh.ch>
2023-05-10 15:13:50 +0200

    Adding bash script logic to install dependencies for installation on new machine

---
+++ dependent_software.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+echo "Installing the xset clipboard for tmux-yank"
+
+sudo apt-get install xset -y
+
+echo "Installing dependencies for pyenv installation"
+
+sudo apt update; sudo apt install build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev curl \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
+echo "Installing pyenv"
+
+curl https://pyenv.run | bash
+
+pyenv install 3.9.12
+pyenv virtualenv 3.9.12 local_env
+
+echo "Installing starship"
+
+curl -sS https://starship.rs/install.sh | sh
+
+echo "Installing kubectl"
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+echo "Installing rust"
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+echo "Installing gitui"
+
+curl -s curl -s https://api.github.com/repos/extrawurst/gitui/releases/latest | grep -wo "https.*linux.*gz" | wget -qi -
+tar xzvf gitui-linux-musl.tar.gz
+rm gitui-linux-musl.tar.gz
+sudo chmod +x gitui
+sudo mv gitui /usr/local/bin
