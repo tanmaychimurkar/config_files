4cbb676aa0ab4891d45468098fa97d9d94c6de72
Tanmay Chimurkar <tanmay.chimurkar@uzh.ch>
2023-05-10 14:01:25 +0200

    Updating JetBrains config message from setup_dotfiles.sh file

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -25,7 +25,7 @@ for file in "${DOTFILES[@]}"; do
     fi
 done
 
-echo "All dotfiles are now copied (except JetBrains configurations). You are good to go!!"
+echo "All dotfiles are now copied (except JetBrains configurations, which need to be copied manually). You are good to go!!"
 echo "The shell will be reloaded automatically now"
 
 exec "$SHELL"
