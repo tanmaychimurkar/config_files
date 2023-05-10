0fa844265cc51ebbb4c9c468e4231ca6a91723c1
Tanmay Chimurkar <tanmay.chimurkar@uzh.ch>
2023-05-10 15:02:36 +0200

    Updating the setup_dotfiles bash scrip

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,7 +1,29 @@
 #!/usr/bin/env bash
 
-# Script to copy dotfiles from the repo to the home directory
 
+function usage {
+  echo "Run using: bash setup_dotfiles.sh [OPTIONS]"
+  echo "Usage: $0 [OPTIONS]"
+  echo "OPTIONS:"
+  echo " --help, -h: Script usage help"
+  echo " --install_deps : Will install dependencies for the dotfiles"
+  echo " --skip_deps : Will skip the installation of the dependencies for the dotfiles"
+}
+
+if [ "$#" -ne 1 ]; then
+  echo "Choose a flag to install dependencies, y/n"
+  echo $'\n'
+  exit 1
+fi
+
+PASSED_ARG="$1"
+
+if [[ "$PASSED_ARG" == "--help" || "$PASSED_ARG" == "-h" ]]; then
+  usage
+  exit 0
+fi
+
+# Script to copy dotfiles from the repo to the home directory
 echo "Configuration files will be copied now to the $HOME directory of this machine"
 
 # Path to the dotfiles repo
@@ -27,7 +49,16 @@ done
 
 echo "All dotfiles are now copied (except JetBrains configurations, which need to be copied manually). You are good to go!!"
 
-source ./dependent_software.sh
+if [[ "$PASSED_ARG" == "--install_deps" ]]; then
+  echo "Installation of dependencies"
+  source ./dependent_software.sh
+elif [[ "$PASSED_ARG" == "--skip_deps" ]]; then
+  echo "Skipping the installation of dependencies"
+else
+  echo "Choose a flag to install either install dependencies, use -h for help"
+  echo $'\n'
+  exit 1
+fi
 
 echo "The shell will be reloaded automatically now"
 
