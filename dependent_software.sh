9cce6e4c88438fec30bde5bd7e548ba5fc3b86fc
root <root@DESKTOP-5O62HDO>
2023-05-11 20:31:39 +0200

    Adding k9s for Kubernetes

--- dependent_software.sh
+++ dependent_software.sh
@@ -39,3 +39,6 @@ tar xzvf gitui-linux-musl.tar.gz
 rm gitui-linux-musl.tar.gz
 sudo chmod +x gitui
 sudo mv gitui /usr/local/bin
+
+echo "Installing k9s for Kubernetes cluster management"
+curl -sS https://webi.sh/k9s | sh
